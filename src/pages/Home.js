c65e737bbe77bca8a1498d3fc9fb5e0cc083cfac
Laert Huti <101653303+laerttt@users.noreply.github.com>
2025-03-25 18:11:32 +0100

    fix home button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,14 +2,22 @@ import "../css/Home.css";
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useTranslation, Trans } from "react-i18next";
+import { Link, useLocation } from 'react-router-dom';
 
 const Home = () => {
   const navigate = useNavigate();
   const { t } = useTranslation();
+  const location = useLocation();
 
   const handleButtonClick = () => {
-    navigate("/services");
+    navigate(`${langPrefix}/services`);
   };
+  const getLanguagePrefix = () => {
+    const lang = location.pathname.split('/')[1];
+    return lang === 'al' || lang === 'en' ? `/${lang}` : '/en';
+  };
+
+  const langPrefix = getLanguagePrefix();
 
   return (
     <div className="home">
