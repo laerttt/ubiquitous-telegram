70a6e0e672358d0f65ef408e00ef6a5be3b94ce9
Laert Huti <101653303+laerttt@users.noreply.github.com>
2025-03-31 02:02:49 +0200

    deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from "react";
-import { BrowserRouter as Router, Route, Routes, useLocation, Navigate } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, useLocation, Navigate } from "react-router-dom";
 import Header from "./components/Header";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
